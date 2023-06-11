013f3c9f79c3706faf904740db2309a3cf58ecc1
JSander72 <jamesearlsanders2@outlook.com>
2023-06-10 20:39:15 -0400

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-$(document).ready(function () { //readied the document
+$(document).ready(function () {
 
-  $("#btnSubmit").attr("disabled", true);//set the submit button as disabled
+  $("#btnSubmit").attr("disabled", true);
   $("<ul></ul>").appendTo("body");
 
   $("#btnSubmit").click(function (event) {//click function
-      event.preventDefault(); //made it so the page does not default to refresh after the form is submitted
-      let inputText = $("#inputText").val();// named the textcontent of the input "inputText"
+      event.preventDefault();
+      let inputText = $("#inputText").val();
       alert(inputText);//will alert the textcontent when button clicked
       $("<div></div>").appendTo("body");//created a div and connected to body
       let li= $("<li>" + inputText + "</li>");
@@ -13,7 +13,7 @@ $(document).ready(function () { //readied the document
 
       li.click(function () {
           var random = "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"
-          li.css("color", random); //I feel like there is a better way to do this...
+          li.css("color", random);
           li.dblclick(function () {
               li.remove();
           });
@@ -21,8 +21,8 @@ $(document).ready(function () { //readied the document
   });
 
 
-  $("body").keydown(function () {//keydown function- when something is typed in the input box the following occurs:
-      let inputText = $("#inputText").val();// named the textcontent of the input "inputText"
+  $("body").keydown(function () {//created a keydown function
+      let inputText = $("#inputText").val();
       if (inputText === "") { //if the inputText is nothing then the button stays disabled
           $("#btnSubmit").prop("disabled", true);
       }
@@ -34,5 +34,5 @@ $(document).ready(function () { //readied the document
 
   //$("h2").hover(function(){//created a mouseover function defining changes to the heading
   //$("h2").css({
-  //"background-color": "purple",
-  //"border-radius": "5px" });
+  //"background-color": "blue",
+  //"border-radius": "6px" });
